d4af93b375098922b72988a8d4c260e12e361204
vlad <vlad@v-lad.org>
2014-08-23 22:40:48 -0400

    Fixed up style and syntax.  Reduce complexity.  Added unit tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var assert = require("assert");
 var git2provCvt = require("../lib/git2provConverter");
 var fs = require('fs');
 var exec = require('child_process').exec;
+var serializers = require('../lib/provSerializer');
 
 
 
@@ -12,6 +13,61 @@ describe('Unit', function(){
       assert.equal(-1, [1,2,3].indexOf(9));
     });
   });
+
+  describe('ProvSerializer ShouldBeAdded', function() {
+      var testColl = { 'a': 1,  'd': 23, 'bill': 9 };
+      var ignoreBlock = "bob;bill,dope";
+      it('Should allow to add property for which we have entry', function() {
+          var a = 'a';
+          var rv = serializers.shouldBeAdded(testColl, ignoreBlock, a, 'a-block');
+          assert.equal(rv, true);
+      } );
+
+      it('Should not-allow to add property which we have in ignore block', function() {
+          var a = 'bill';
+          var rv = serializers.shouldBeAdded(testColl, ignoreBlock, a, 'bill');
+          assert.equal(rv, false);
+      } );
+
+      it('Should not-allow to add property which is not in a collection', function() {
+          var a = 'bruce';
+          var rv = serializers.shouldBeAdded(testColl, ignoreBlock, a, 'bruce');
+          assert.equal(rv, false);
+      } );
+  });
+
+  describe('Add to JSON bundle', function() {
+
+      var jSONObj;
+      var ignoreBlock = "bob;dope";
+      var provObj = { 'bills': [1,2,3], 'bundle': 'bill' };
+
+      beforeEach(function(complete) {
+          jSONObj =  {'bundle': { 'bill': {}}};
+          complete();
+      });
+
+      it('Should add bundle to JSON object', function() {
+
+          serializers.addToJSONBundleIfNeeded(jSONObj,provObj,ignoreBlock,'bills','bill');
+          assert.deepEqual({bill: [1,2,3]}, jSONObj.bundle.bill);
+
+      } );
+
+      it('Should add bundle to JSON object if set is ignored', function() {
+
+          serializers.addToJSONBundleIfNeeded(jSONObj,provObj,"bill",'bills','bill');
+          assert.deepEqual({}, jSONObj.bundle.bill);
+
+      } );
+
+      it('Should not add bundle to JSON object if receptacle is not there', function() {
+          serializers.addToJSONBundleIfNeeded(jSONObj,provObj,ignoreBlock,'drews','drew');
+          assert.deepEqual({}, jSONObj.bundle.bill);
+
+      } );
+  });
+
 });
 
 
